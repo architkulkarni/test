09d96331da361549806ae9e5b36f2778babeec9b
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2024-03-27 13:19:06 -0700

    Change flash-attn version to 2.3.3
    Signed-off-by: Archit Kulkarni <architkulkarni@users.noreply.github.com>

--- entrypoint.py
+++ entrypoint.py
@@ -1,9 +1,9 @@
 import ray
-ray.init(runtime_env={"pip": ["flash-attn==2.3.6"]})
+ray.init(runtime_env={"pip": ["flash-attn==2.3.3"]})
 
 # Launch a task
 @ray.remote
 def f():
-    print("Successfully installed flash-attn==2.3.6")
+    print("Successfully installed flash-attn==2.3.3 for task")
 
 ray.get(f.remote())
